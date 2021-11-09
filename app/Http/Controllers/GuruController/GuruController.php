a039006ce4edef282948bdfd49b3b91fa232e523
adittiyaMaulana <adittiyam250@gmail.com>
2021-11-09 21:54:04 +0700

    init migration and controller

---
+++ app/Http/Controllers/GuruController/GuruController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\GuruController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GuruController extends Controller
+{
+    //
+}
---
+++ database/migrations/2021_11_09_144444_create_informasi_sekolah_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformasiSekolahTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informasi_sekolah', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informasi_sekolah');
+    }
+}
